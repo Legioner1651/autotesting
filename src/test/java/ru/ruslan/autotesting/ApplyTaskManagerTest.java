5ed2d187d858d59467b875b643d46477c0c34028
Ruslan <ruspet55@ngs.ru>
2025-10-05 16:03:07 +0700

    Environment, Parameters

--- src/test/java/ru/ruslan/autotesting/ApplyTaskManagerTest.java
+++ src/test/java/ru/ruslan/autotesting/ApplyTaskManagerTest.java
@@ -27,48 +27,6 @@ public class ApplyTaskManagerTest extends ApplyTaskManagerBase {
         String testYML = paramInstance.getProperty("topics.topic1");
         System.out.println("testYML = \"" + testYML + "\"");
 
-        // Настройка producer для отправки сообщений в Kafka
-        Properties producerProps = new Properties();
-        producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers());
-        producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        producerProps.put(ProducerConfig.CLIENT_ID_CONFIG, "producer-id");
-
-        KafkaProducer<String, String> producer = new KafkaProducer<>(producerProps);
-
-        // Отправляем сообщение в топик 'topic1'
-        ProducerRecord<String, String> record = new ProducerRecord<>(
-                paramInstance.getProperty("topics.topic1"), // Извлекаем имя топика из YML-файла
-                "Hello world!!! OK 1"
-        );
-        producer.send(record).get();
-        producer.close();
-
-        // Чтение сообщений из Kafka
-        Properties consumerProps = new Properties();
-        consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaContainer.getBootstrapServers());
-        consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, "group-id");
-        consumerProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, OffsetResetStrategy.EARLIEST.name().toLowerCase());
-        consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(consumerProps);
-        consumer.subscribe(Collections.singleton(paramInstance.getProperty("topics.topic1")));
-
-        ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
-        for (ConsumerRecord<String, String> r : records) {
-            log.info("Received message from Kafka: {}", r.value()); // выводим полученное сообщение
-        }
-        consumer.close();
-
-        // Выполняем простой SQL-запрос к PostgreSQL
-        Connection connection = postgresqlContainer.createConnection("");
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM pg_database;");
-        while(resultSet.next()){
-            log.info("Result of DB Query: {}", resultSet.getString(1)); // выводим результат запроса
-        }
-
         // Читаем значение параметра из application.yml
         String configValue = readConfigValueFromYML();
         log.info("Config value from YAML file is: {}", configValue); // выводим значение параметра
