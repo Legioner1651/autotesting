28a4eb6a9c1e62f2ad83676083c776464de3f19c
Ruslan <ruslan551@ro.ru>
2025-10-06 20:04:05 +0700

    Param.getEnv() - работает в классе ApplyTaskManagerTest Param.getProperty() - не работает

--- src/test/java/ru/ruslan/autotesting/ApplyTaskManagerTest.java
+++ src/test/java/ru/ruslan/autotesting/ApplyTaskManagerTest.java
@@ -1,50 +1,17 @@
 package ru.ruslan.autotesting;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
 
-public class ApplyTaskManagerTest {
+class ApplyTaskManagerTest {
 
     @Test
-    public void testMethod() {
-        System.out.println("*** ===  @Test  === Start === ***");
-        String value1 = Param.getProperty("some.config.key1");
-        System.out.println("some.config.key1: " + value1);
-        String env = Param.getEnv("SSH_AUTH_SOCK");
-        System.out.println("env SSH_AUTH_SOCK: " + env);
-        System.out.println("*** ===  @Test  === End === ***");
+    void testPropertiesAndEnvironment() {
+        // Используем статический метод для получения свойства из файла настроек
+        String ymlValue = Param.getProperty("some.config.key1");
+        System.out.println("Значение из YML: " + ymlValue);
+
+        // Используем статический метод для получения переменной окружения
+        String sshAuthSock = Param.getEnv("USERNAME");
+        System.out.println("Переменная окружения SSH_AUTH_SOCK: " + sshAuthSock);
     }
 }
-
-//    @BeforeAll
-//    public static void beforeAll() {
-//        System.out.println("=== @BeforeAll === Start ===");
-//        // Вызов методов
-//        GeneralMethods.printSomeConfigKey();
-//        System.out.println("env SSH_AUTH_SOCK: " + Param.getEnv("SSH_AUTH_SOCK"));
-//        System.out.println("=== @BeforeAll === End ===");
-//    }
-//
-//    @BeforeEach
-//    public void beforeEach() {
-//        System.out.println("=== @BeforeEach === Start ===");
-//        GeneralMethods.printSomeConfigKey();
-//        System.out.println("env SSH_AUTH_SOCK: " + Param.getEnv("SSH_AUTH_SOCK"));
-//        System.out.println("=== @BeforeEach === End ===");
-//    }
-//
-//    @AfterEach
-//    public void afterEach() {
-//        System.out.println("=== @AfterEach === Start ===");
-//        GeneralMethods.printSomeConfigKey();
-//        System.out.println("env SSH_AUTH_SOCK: " + Param.getEnv("SSH_AUTH_SOCK"));
-//        System.out.println("=== @AfterEach === End ===");
-//    }
-//
-//    @AfterAll
-//    public static void afterAll() {
-//        System.out.println("=== @AfterAll === Start ===");
-//        GeneralMethods.printSomeConfigKey();
-//        System.out.println("env SSH_AUTH_SOCK: " + Param.getEnv("SSH_AUTH_SOCK"));
-//        System.out.println("=== @AfterAll === End ===");
-//    }
-//}
--- src/test/java/ru/ruslan/autotesting/Param.java
+++ src/test/java/ru/ruslan/autotesting/Param.java
@@ -1,66 +1,36 @@
 package ru.ruslan.autotesting;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.core.env.StandardEnvironment;
+import org.springframework.stereotype.Component;
 
-import java.util.*;
-
+@Component
 public class Param {
 
-    private static Environment environment;
+    private static volatile Environment environment;
 
-    static {
-        // Инициализация Spring среды без Spring Boot ApplicationContext
-        environment = SpringContextLoader.getEnvironment();
-    }
-
-    // Получение свойства из application.yml или environment
-    public static String getProperty(String key) {
-        return environment.getProperty(key);
-    }
-
-    // Получение списка длинных имен топиков
-    public static List<String> getTopicNames() {
-        Map<String, String> topics = getTopicsMap();
-        List<String> longNames = new ArrayList<>();
-        for (Map.Entry<String, String> entry : topics.entrySet()) {
-            if (entry.getValue().length() > 4) { // условие "длинное имя"
-                longNames.add(entry.getValue());
+    @Autowired
+    public void setEnvironment(Environment env) {
+        if (environment == null) { // Инициализация однократно
+            synchronized (this.getClass()) {
+                if (environment == null)
+                    environment = env;
             }
         }
-        return longNames;
     }
 
-    // Получение количества пар коротких и длинных имен топиков
-    public static int getTopicsizes() {
-        Map<String, String> topics = getTopicsMap();
-        return topics.size();
+    /**
+     * Получение значения параметра из конфигурационного файла application.yml
+     */
+    public static String getProperty(String propertyName) {
+//        return environment.getProperty(propertyName);
+        return "--- Заглушка для метода getProperty() ---";
     }
 
-    // Вспомогательный метод для получения всех топиков
-    private static Map<String, String> getTopicsMap() {
-        Map<String, String> topicsMap = new HashMap<>();
-        String prefix = "topics.";
-        String topicsPrefix = "topics.";
-        // так как мы не используем Spring Boot ApplicationContext
-        // и не можем напрямую получать из environment,
-        // парсим YAML вручную или сделаем через System.getenv()
-        // Для этого примера лучше считать из environment или статичных данных
-        // но поскольку в рамках задания строго, сделаем так:
-        // Для простоты объявим статический Map внутри класса:
-
-        // В реальной ситуации лучше было бы загрузить YAML через SnakeYAML или Spring yaml resolver
-        // Но для упрощения:
-        Map<String, String> staticTopics = new HashMap<>();
-        staticTopics.put("topic1", "topic-test-1");
-        staticTopics.put("topic2", "topic-test-2");
-        staticTopics.put("topic3", "topic-test-3");
-        staticTopics.put("topic4", "topic-test-4");
-        return staticTopics;
-    }
-
-    // Получение значения переменных окружения
-    public static String getEnv(String envVarName) {
-        return System.getenv(envVarName);
+    /**
+     * Получение значения переменной окружения по её имени
+     */
+    public static String getEnv(String variableName) {
+        return System.getenv(variableName); // Доступ к переменным окружения
     }
 }
