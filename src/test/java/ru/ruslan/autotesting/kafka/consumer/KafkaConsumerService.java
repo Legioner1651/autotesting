61bf452694b27c123301cf0c4d2733b21359de3c
Legioner1651 <113102875+Legioner1651@users.noreply.github.com>
2025-10-12 19:13:25 +0700

    Update KafkaConsumerService.java

--- src/test/java/ru/ruslan/autotesting/kafka/consumer/KafkaConsumerService.java
+++ src/test/java/ru/ruslan/autotesting/kafka/consumer/KafkaConsumerService.java
@@ -1,4 +1,45 @@
 package ru.ruslan.autotesting.kafka.consumer;
 
+import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
+import io.qameta.allure.model.StatusDetails;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import java.util.Collections;
+import java.util.Properties;
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+
 public class KafkaConsumerService {
+
+    private final Properties consumerProps;
+
+    public KafkaConsumerService(Properties props) {
+        this.consumerProps = props;
+    }
+
+    @Step("Получение сообщения из топика {topic}")
+    public <K, V> CompletableFuture<ConsumerRecord<K, V>> consumeMessage(String topic) {
+        CompletableFuture<ConsumerRecord<K, V>> future = new CompletableFuture<>();
+
+        try (KafkaConsumer<K, V> consumer = new KafkaConsumer<>(consumerProps)) {
+            consumer.subscribe(Collections.singletonList(topic));
+
+            while (!future.isDone()) {
+                ConsumerRecords<K, V> records = consumer.poll(Duration.ofMillis(100)); // Опрашиваем каждые 100 мс
+
+                for (ConsumerRecord<K, V> record : records) {
+                    future.complete(record); // Завершаем выполнение с результатом
+
+                    Allure.getLifecycle().updateStep(stepResult ->
+                            stepResult.setStatusDetails(new StatusDetails().setMessage(record.toString()))
+                    );
+                }
+            }
+        }
+
+        return future;
+    }
 }
+
