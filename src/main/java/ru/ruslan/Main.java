5ec2719e524cc1c0f01c62ed3dc9ff137767c8db
Ruslan <ruslan551@ro.ru>
2025-10-30 21:56:14 +0700

    add test whis meetting

--- src/main/java/ru/ruslan/Main.java
+++ src/main/java/ru/ruslan/Main.java
@@ -1,7 +1,28 @@
 package ru.ruslan;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        Set<Runnable> sourceList = new HashSet<>();
+        IClass item = new IClass();
+
+        sourceList.add(item::exec);
+        sourceList.add(null);
+        sourceList.add(item::exec);
+        sourceList.add(item::exec);
+
+        for( Runnable element: sourceList ) {
+            if (element != null) {
+                element.run();
+                sourceList.remove(element);
+            }
+        }
+
+        System.out.println( sourceList.size() );
     }
 }
