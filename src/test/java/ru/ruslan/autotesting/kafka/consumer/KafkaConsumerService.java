f504fe0122653ed226d011f8a07438e1d3acb9d8
Ruslan <ruspet55@ngs.ru>
2025-11-03 13:04:10 +0700

    Рабочий вариант использования org.testcontainers.kafka.KafkaContainer; и Kafka-UI

---
+++ src/test/java/ru/ruslan/autotesting/kafka/consumer/KafkaConsumerService.java
@@ -0,0 +1,57 @@
+package ru.ruslan.autotesting.kafka.consumer;
+
+import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
+import io.qameta.allure.model.StatusDetails;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.*;
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+public class KafkaConsumerService {
+
+    private final Properties consumerProps;
+
+    public KafkaConsumerService(Properties props) {
+        this.consumerProps = props;
+    }
+
+    @Step("Получение сообщения из топика {topic}")
+    public <K, V> List<Map.Entry<K, V>> ConsumerAllRecords(String topic) {
+
+        List<Map.Entry<K, V>> recordList = new ArrayList<>();
+
+        try (KafkaConsumer<K, V> consumer = new KafkaConsumer<>(consumerProps)) {
+            consumer.subscribe(Collections.singletonList(topic));
+
+            boolean hasMoreMessages = true;
+
+            while (hasMoreMessages) {
+                ConsumerRecords<K, V> records = consumer.poll(Duration.ofMillis(300)); // Опрашиваем каждые 300 мс
+
+                log.info("records.count() = {}", records.count());
+
+                for (ConsumerRecord<K, V> record : records) {
+                    K key = record.key();
+                    V value = record.value();
+
+                    recordList.add(Map.entry(key, value));
+                    log.info("ConsumerAllRecords Key:Value => \"{}\" : \"{}\"", key, value);
+
+                    Allure.getLifecycle().updateStep(stepResult ->
+                            stepResult.setStatusDetails(new StatusDetails().setMessage(record.toString()))
+                    );
+                }
+                /* Пока не получим топики, будем снова пытаться получить */
+                hasMoreMessages = records.isEmpty();
+            }
+        }
+
+        return recordList;
+    }
+}
---
+++ src/test/java/ru/ruslan/autotesting/kafka/producer/MessageSender.java
@@ -0,0 +1,119 @@
+package ru.ruslan.autotesting.kafka.producer;
+
+import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
+import io.qameta.allure.model.StatusDetails;
+import org.apache.kafka.clients.producer.*;
+import org.testcontainers.shaded.org.awaitility.Awaitility;
+
+import java.time.Duration;
+import java.util.Properties;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+public class MessageSender {
+
+    private final Properties producerProps;
+
+    /**
+     * Конструктор принимает конфигурационные свойства производителя Kafka.
+     */
+    public MessageSender(Properties producerProps) {
+        this.producerProps = producerProps;
+    }
+
+    @Step("Отправка сообщения в топик {topic}")
+    public <K, V> CompletableFuture<SendResult<K, V>> sendMessage(
+            String topic,
+            K key,
+            V value
+    ) throws ExecutionException, InterruptedException {
+        try (KafkaProducer<K, V> producer = new KafkaProducer<>(producerProps)) {
+            ProducerRecord<K, V> record = new ProducerRecord<>(topic, key, value);
+            Future<RecordMetadata> future = producer.send(record);
+
+            // Ждем завершения отправки записи
+            awaitSendCompletion(future);
+
+            RecordMetadata metadata = future.get();
+
+            // Формируем результат самостоятельно
+            SendResult<K, V> result = new SendResult<>(
+                    metadata.topic(),
+                    metadata.partition(),
+                    metadata.offset(),
+                    metadata.timestamp(), // Здесь доступна временная метка
+                    key,
+                    value
+            );
+
+            Allure.getLifecycle().updateStep(stepResult ->
+                    stepResult.setStatusDetails(new StatusDetails().setMessage(metadata.toString())));
+            return CompletableFuture.completedFuture(result);
+        }
+    }
+
+    /**
+     * Метод ожидает завершения отправки сообщения.
+     *
+     * @param future Будущее выполнение отправки записи
+     */
+    private void awaitSendCompletion(Future<?> future) {
+        Awaitility.await()
+                .timeout(Duration.ofMinutes(11))
+                .pollInterval(100, TimeUnit.MILLISECONDS)
+                .atMost(5, TimeUnit.SECONDS)
+//                .until(() -> future.isDone() && !future.isCompletedExceptionally());
+                .until(future::isDone);
+    }
+
+    /**
+     * Класс для хранения результатов отправки сообщения.
+     */
+    public static class SendResult<K, V> {
+        private final String topic;
+        private final int partition;
+        private final long offset;
+        private final long timestamp;
+        private final K key;
+        private final V value;
+
+        public SendResult(String topic, int partition, long offset, long timestamp, K key, V value) {
+            this.topic = topic;
+            this.partition = partition;
+            this.offset = offset;
+            this.timestamp = timestamp;
+            this.key = key;
+            this.value = value;
+        }
+
+        // Методы геттеры...
+
+        // Геттеры для всех полей:
+        public String getTopic() {
+            return topic;
+        }
+
+        public int getPartition() {
+            return partition;
+        }
+
+        public long getOffset() {
+            return offset;
+        }
+
+        public long getTimestamp() {
+            return timestamp;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+    }
+}
