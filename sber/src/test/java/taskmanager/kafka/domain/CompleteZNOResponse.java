7379e130765d13a6d77f4961db763d18eb9cb1e3
Legioner1651 <113102875+Legioner1651@users.noreply.github.com>
2025-12-19 20:57:29 +0700

    filling in the code

--- sber/src/test/java/taskmanager/kafka/domain/CompleteZNOResponse.java
+++ sber/src/test/java/taskmanager/kafka/domain/CompleteZNOResponse.java
@@ -1,4 +1,18 @@
 package taskmanager.kafka.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CompleteZNOResponse {
+    private long rowId;
+    private String smResponse;
+    private String commentSmResponse;
+    private Instant createdAt;
 }
+
