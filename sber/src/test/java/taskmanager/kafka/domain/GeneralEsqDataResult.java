8d7ec5bd8b28f2230ac1ab63a81e463abad3dbba
Legioner1651 <113102875+Legioner1651@users.noreply.github.com>
2025-12-19 21:06:20 +0700

    filling in the code

--- sber/src/test/java/taskmanager/kafka/domain/GeneralEsqDataResult.java
+++ sber/src/test/java/taskmanager/kafka/domain/GeneralEsqDataResult.java
@@ -1,4 +1,17 @@
 package taskmanager.kafka.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GeneralEsqDataResult {
+    private long esqId;
+    private Boolean processed;
+    private String status;
+    private String resultComment;
 }
