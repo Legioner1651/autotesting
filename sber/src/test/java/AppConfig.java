05f82a27a2e16b063910ce198a40e5c5292c40bc
Legioner1651 <113102875+Legioner1651@users.noreply.github.com>
2025-12-19 21:23:33 +0700

    filling in the code

--- sber/src/test/java/AppConfig.java
+++ sber/src/test/java/AppConfig.java
@@ -1,2 +1,9 @@
+package ru.sberbank.sberuser.surms.test.integration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({Param.class})
 public class AppConfig {
 }
