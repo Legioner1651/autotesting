53285c89cf6769122c4f9530592e17edabd702b7
Legioner1651 <113102875+Legioner1651@users.noreply.github.com>
2025-12-19 21:20:19 +0700

    filling in the code

--- sber/src/test/java/taskmanager/kafka/KafkaJsonSerializer.java
+++ sber/src/test/java/taskmanager/kafka/KafkaJsonSerializer.java
@@ -1,4 +1,24 @@
 package taskmanager.kafka;
 
-public class KafkaJsonSerializer {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Serializer;
+
+public class KafkaJsonSerializer<T> implements Serializer<T> {
+
+    private final ObjectMapper objectMapper = new ObjectMapper()
+            .registerModule(new JavaTimeModule()); // добавлена регистрация модуля для обработки дат
+
+    @Override
+    public byte[] serialize(String topic, T data) {
+        if (data == null)
+            return null;
+
+        try {
+            return objectMapper.writeValueAsBytes(data);
+        } catch (Exception ex) {
+            throw new SerializationException("Ошибка сериализации объекта в JSON", ex);
+        }
+    }
 }
