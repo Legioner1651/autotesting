740934785e0fb1998558397abb9d365a72122ce0
Legioner1651 <113102875+Legioner1651@users.noreply.github.com>
2025-12-19 20:59:56 +0700

    filling in the code

--- sber/src/test/java/taskmanager/kafka/domain/CreateSDResponse.java
+++ sber/src/test/java/taskmanager/kafka/domain/CreateSDResponse.java
@@ -1,4 +1,19 @@
 package taskmanager.kafka.domain;
+package ru.sberbank.sberuser.surms.test.integration.apply.taskmanager.kafka.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateSDResponse {
+    private long rowId;
+    private String smResponse;
+    private String commentSmResponse;
+    private String objectId;
+    private Instant createdAt;
 }
